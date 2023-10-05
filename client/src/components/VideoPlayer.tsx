b62a7c9d7b7b72b7faf9bd704d14e5d26b2bdae2
1willcobb <cobb.will@gmail.com>
2023-10-04 22:23:33 -0700

    multi videosworking

--- client/src/components/VideoPlayer.tsx
+++ client/src/components/VideoPlayer.tsx
@@ -1,20 +1,45 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
+import { useState } from "react";
 import ReactPlayer from "react-player";
 
+// const style = {
+//   display: "flex",
+// };
 
-const style = {
-  display: "flex",
-  height: "100vh",
-  justifyContent: "center",
-  alignItems: "center",
-}
+const root = "https://d2amnd3jkfva2l.cloudfront.net/";
+
+const videos = [
+  "Jake%20&%20Brittany%20feature.mp4",
+  "pt1.mp4",
+  "pt2.mp4",
+  "pt3.mp4",
+  "pt4.mp4",
+];
 
 function VideoPlayer() {
+  const [videoUrl, setVideoUrl] = useState("");
+
+  const handleVideoSelect = (videoKey: string) => {
+    setVideoUrl(root + videoKey);
+  };
+
 
   return (
-    <div style={style}>
-      <ReactPlayer height={"1000px"} width={"1000px"} url="https://d2amnd3jkfva2l.cloudfront.net/Jake%20&%20Brittany%20feature.mp4" controls={false} playing={true} loop={true} muted/>    
+    <div>
+      {videos.map((video) => (
+        <>
+          <button key={video} onClick={()=>handleVideoSelect(video)}>{video}</button>
+        </>
+      ))}
+      {videoUrl && 
+      <ReactPlayer
+        height={"500px"}
+        width={"500px"}
+        url={videoUrl}
+        controls={false}
+        playing={true}
+        loop={true}
+        muted
+      /> }
     </div>
   );
 }
